042ed94c230a2481ede2d79c9fe2662ab189e9f9
Stanley Pan <stanleypan.egre@gmail.com>
2016-07-12 00:52:05 +0900

    Update API Key, add methodParameter dictionary from constants

--- FlickFinder/Constants.swift
+++ FlickFinder/Constants.swift
@@ -35,7 +35,7 @@ struct Constants {
     // Flickr Parameter Values
     struct FlickrParameterValues {
         static let SearchMethod = "flickr.photos.search"
-        static let APIKey = "YOUR_API_KEY_HERE"
+        static let APIKey = "39057e4202a0bc4cb3c700598a4df3ab"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
